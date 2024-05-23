aa6fff25a9d6947dc65fbd2945add65eba302da9
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2024-05-23 07:55:03 -0300

    feat: Creating the ContatoController controller and your Create method

---
+++ API-Contato/Controllers/ContatoController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Contato.Context;
+using API_Contato.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API_Contato.Controllers
+{
+
+    [ApiController]
+    [Route("[Controller]")]
+    public class ContatoController : ControllerBase
+    {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);
+        }
+    }
+}
